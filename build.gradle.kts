d013bfdaad2371b009f76b84ba39ca20c80e3df5
Zax71 <zabax32@gmail.com>
2024-10-28 16:48:21 +0000

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,44 +11,39 @@ version = "1.0.0"
 repositories {
     mavenCentral()
     maven("https://jitpack.io")
-
-    // Adventure dev builds
-    maven(url = "https://s01.oss.sonatype.org/content/repositories/snapshots/") {
-        name = "sonatype-oss-snapshots"
-    }
 }
 
 dependencies {
 
     // Minestom
-    implementation("net.minestom:minestom-snapshots:f1d5940855")
+    implementation("net.minestom:minestom-snapshots:dba90a461b")
 
     // Polar
-    implementation("dev.hollowcube:polar:1.10.0")
+    implementation("dev.hollowcube:polar:1.11.3")
 
     // GoldenStack/window
-    implementation("com.github.GoldenStack:window:7617978")
+    implementation("com.github.GoldenStack:window:a2feac67eb")
 
 
     // Kyori stuff (Adventure)
-    implementation("net.kyori:adventure-text-serializer-plain:4.16.0")
-    implementation("net.kyori:adventure-text-minimessage:4.16.0")
+    implementation("net.kyori:adventure-text-serializer-plain:4.17.0")
+    implementation("net.kyori:adventure-text-minimessage:4.17.0")
     implementation("net.kyori:adventure-text-serializer-ansi:4.17.0")
 
     // Configuration API
     implementation("org.spongepowered:configurate-hocon:4.1.2")
 
     // Jedis (Redis lib)
-    implementation("redis.clients:jedis:5.1.1")
+    implementation("redis.clients:jedis:5.2.0")
 
     // Discord lib
     implementation("club.minnced:discord-webhooks:0.8.4")
 
     // Logger
-    implementation("ch.qos.logback:logback-classic:1.5.6")
+    implementation("ch.qos.logback:logback-classic:1.5.12")
 
     // Apache Commons Lang
-    implementation("org.apache.commons:commons-lang3:3.14.0")
+    implementation("org.apache.commons:commons-lang3:3.17.0")
 
 
 }
