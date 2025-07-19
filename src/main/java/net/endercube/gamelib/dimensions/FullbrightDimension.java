8cf2dd7a5f2efdbee6974ef637ee03699fc8dccd
Zax71 <67716263+zax71@users.noreply.github.com>
2025-07-19 13:49:33 +0100

    Use Adventure `Key.Key()` when registering dimension

--- src/main/java/net/endercube/gamelib/dimensions/FullbrightDimension.java
+++ src/main/java/net/endercube/gamelib/dimensions/FullbrightDimension.java
@@ -1,5 +1,6 @@
 package net.endercube.gamelib.dimensions;
 
+import net.kyori.adventure.key.Key;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.registry.RegistryKey;
 import net.minestom.server.world.DimensionType;
@@ -9,9 +10,14 @@ public class FullbrightDimension {
     public static final RegistryKey<DimensionType> INSTANCE;
 
     static {
-        INSTANCE = MinecraftServer.getDimensionTypeRegistry().register("endercube:full_bright", DimensionType.builder()
-                .ambientLight(2.0f)
-                .build());
+        INSTANCE = MinecraftServer
+                .getDimensionTypeRegistry()
+                .register(
+                        Key.key("endercube:full_bright"),
+                        DimensionType.builder()
+                                .ambientLight(2.0f)
+                                .build()
+                );
     }
 
 }
